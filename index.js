ae4cb2074723513fd6d8546d2925a782891646db
Steren <steren.giannini@gmail.com>
2023-02-06 20:47:05 +0000

    Properly capture request body

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ import * as http from 'http';
 const serviceName = process.env.K_SERVICE || "log-request";
 const port = process.env.PORT || 8080;
 
-const server = http.createServer(async (req, res) => {
-  const payload = {
-    method: req.method,
-    path: req.path,
-    host: req.host,
-    protocol: req.protocol,
-    headers: req.headers,
-    body: req.body,
-  }
-
+function returnPayload(response, payload) {
   console.log(`${serviceName} received a request:`);
   console.log(JSON.stringify(payload));
-  return res.end(JSON.stringify(payload));
+  response.end(JSON.stringify(payload));
+}
+
+const server = http.createServer(async (request, response) => {
+  const payload = {
+    method: request.method,
+    url: request.url,
+    headers: request.headers,
+  }
+
+  if(request.method === 'POST') {
+    let body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      payload.body = body;
+      returnPayload(response, payload);
+    });
+  } else {
+    return returnPayload(response, payload);
+  }
 });
 server.listen(port, () => console.info(`${serviceName} is listening on port ${port}`));
